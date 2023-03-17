a2b49e5f52d7c9de40d49138a14b19aa99e04684
iThieler <39486895+iThieler@users.noreply.github.com>
2023-03-17 19:58:46 +0100

    Create variables.sh

---
+++ reqs/variables.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+var_githubraw="https://raw.githubusercontent.com/iThieler/uscp"
+var_logfile="/root/log_iThielers_USCP-Script.txt"
+var_whipbacktitle="© 2023 - iThieler's Ultimate Server Controllpanel (USCP)"
+
+################################
+##        C O L O R S         ##
+################################
+var_color_nc='\033[0m'
+var_color_red='\033[1;31m'
+var_color_green='\033[1;32m'
+var_color_yellow='\033[1;33m'
+var_color_blue='\033[1;34m'
