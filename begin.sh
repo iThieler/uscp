e844d998bb485cb0c19ba8f147941534cd330f0e
iThieler <39486895+iThieler@users.noreply.github.com>
2023-03-17 19:58:59 +0100

    Update begin.sh

--- begin.sh
+++ begin.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
 source <(curl -s https://raw.githubusercontent.com/iThieler/uscp/main/reqs/functions.sh)
+source <(curl -s https://raw.githubusercontent.com/iThieler/uscp/main/reqs/variables.sh)
+langlist=$(curl -s ${var_githubraw}/main/lang/list)
+language=$(whiptail --menu --nocancel --backtitle "${var_whipbacktitle}" "\nSelect your Language" 20 80 10 "${langlist[@]}" 3>&1 1>&2 2>&3)
+
+
+
