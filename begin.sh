022de62f11fee3dbf03a5a70dadca5e2b2d62bac
iThieler <39486895+iThieler@users.noreply.github.com>
2023-03-28 11:29:43 +0200

    Update begin.sh

--- begin.sh
+++ begin.sh
@@ -96,55 +96,18 @@ if ! ping -c1 -W5 google.com &> /dev/null; then
   exit 1
 fi
 
+# Check Answer-File
 if [ -f "$var_answerfile" ]; then
+  EchoLog info "${lang_answerfile_found}"
   source "$var_answerfile"
 else
-  ###############################
-  ## Q U E S T I O N N A I R E ##
-  ###############################
-  # Hostname
-  HostName=$(WhipInputbox "${lang_hostname_title}" "${lang_hostname_message}" "${GivenHostName}")
-  DomainName=$(WhipInputbox "${lang_domainname_title}" "${lang_domainname_message}" "${GivenDomainName}")
-
-  # Mailserver for notifications
-  WhipMessage "${lang_mailserver_title}" "${lang_mailserver_messageboxtext}"
-  MailServerFQDN=$(WhipInputbox "${lang_mailserver_title}" "${lang_mailserver_mailserverfqdntext}" "mail.${DomainName}")
-  MailServerPort=$(WhipInputbox "${lang_mailserver_title}" "${lang_mailserver_mailserverporttext}" "587")
-  MailServerFrom=$(WhipInputbox "${lang_mailserver_title}" "${lang_mailserver_mailserverfromtext}" "notify@${DomainName}")
-  MailServerTo=$(WhipInputbox "${lang_mailserver_title}" "${lang_mailserver_mailservertotext}" "monitor@${DomainName}")
-  MailServerUser=""
-  MailServerPass=""
-  MailServerTLS=false
-  if WhipYesNo "${lang_btn_yes}" "${lang_btn_no}" "${lang_mailserver_title}" "${lang_mailserver_needloginyesnotext}"; then
-    MailServerUser=$(WhipInputbox "${lang_mailserver_title}" "${lang_mailserver_needloginusertext}" "${MailServerFrom}")
-    MailServerPass=$(WhipInputboxPassword "${lang_mailserver_title}" "${lang_mailserver_needloginpasstext}" "")
-    if WhipYesNo "${lang_btn_yes}" "${lang_btn_no}" "${lang_mailserver_title}" "${lang_mailserver_needloginsecuretext}"; then MailServerTLS=true; fi
+  if bash <(curl -s https://raw.githubusercontent.com/iThieler/uscp/main/misc/mc.sh); then
+    EchoLog ok "${lang_answerfile_genrate_ok}"
+    source "$var_answerfile"
+  else
+    EchoLog error "${lang_answerfile_generate_error}"
+    exit 1
   fi
-
-  # Answer File
-cat > "$var_answerfile" <<EOF
-#!/bin/bash
-########### ANSWERFILE - iThieler' ULTIMATE SERVER CONTROL PANEL SCRIPT ###########
-## In this file, responses are stored in variables that were made when the user  ##
-## executed the script. This makes it easier to re-run the script because the    ##
-## user does not have to answer all the questions over and over again.           ##
-##                                                                               ##
-## Created on $(date)                                                            ##
-###################################################################################
-# Host Variables
-HostName="${HostName}"
-DomainName="${DomainName}"
-FullName="${HostName}.${DomainName}"
-
-# Mailserver Variables
-MailServerFQDN="${MailServerFQDN}"
-MailServerPort=${MailServerPort}
-MailServerFrom="${MailServerFrom}"
-MailServerTo="${MailServerTo}"
-MailServerUser="${MailServerUser}"
-MailServerPass="${MailServerPass}"
-MailServerTLS=${MailServerTLS}
-EOF
 fi
 
 ###############################
