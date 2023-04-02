9f8d9056d21036b1628a63b0527c22fd861b814f
iThieler <39486895+iThieler@users.noreply.github.com>
2023-04-02 02:59:47 +0200

    Update mp.sh

--- misc/mp.sh
+++ misc/mp.sh
@@ -1,1 +1,10 @@
 #!/bin/bash
+# Load functions/updates and strt this script
+source <(curl -s ${var_githubraw}/main/reqs/functions.sh)
+source <(curl -s ${var_githubraw}/main/lang/${language}.sh)
+if [ -f "$var_answerfile" ]; then source "$var_answerfile"; fi
+echo; MailcowLogo; echo
+
+if ! CheckDNS "${FullName}"; then exit 1; fi
+
+exit 0
