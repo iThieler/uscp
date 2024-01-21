bb7d6d99ac0fa305c589fb5ed92ff0ee0de8d338
iThieler <39486895+iThieler@users.noreply.github.com>
2024-01-21 08:23:33 +0100

    Update begin.sh
    Make a clean Mailcow setup

--- begin.sh
+++ begin.sh
@@ -42,7 +42,6 @@ langlist=(\
 # Lists for Serverroles
 rolelist=(\
   "dp" "      Docker Server with Proxy" \
-  "mc" "      Mailcow Mailserver" \
   "mp" "      MailPiler Mailarchiv" \
   "nb" "      NetBox" \
   "om" "      TP-Link Omada Software Controller" \
@@ -112,6 +111,9 @@ else
   fi
 fi
 
+# Ask the user whether the server should be used as a mail server with Mailcow.
+if WhipYesNo "${lang_btn_yes}" "${lang_btn_no}" "${lang_configmailserver_title}" "${lang_configmailserver_messageyesno}"; then ConfigMailserverRole=true; fi
+
 ###############################
 ##       S T A R T U P       ##
 ###############################
@@ -128,40 +130,42 @@ if [[ $(cat /etc/hostname) != "$HostName.$DomainName" ]]; then
   sed -i "s/127.0.1.1 .*/127.0.1.1 $HostName $HostName.$DomainName/" /etc/hosts
 fi
 
-EchoLog wait "${lang_wait_installsoftware}"
-
-# Install and configure Postfix as MTA
-if CheckPackage "postfix"; then
-  EchoLog info "postfix - ${lang_softwaredependencies_alreadyinstalled}"
-  if CheckPackage "mailutils"; then EchoLog info "mailutils - ${lang_softwaredependencies_alreadyinstalled}"; fi
-else
-  debconf-set-selections <<< "postfix postfix/mailname string $HostName.$DomainName"
-  debconf-set-selections <<< "postfix postfix/main_mailer_type string 'Internet Site'"
-  if apt-get install -y postfix >/dev/null 2>&1; then
-    EchoLog ok "postfix - ${lang_softwaredependencies_installok}"
-    if apt-get install -y mailutils >/dev/null 2>&1; then
-      EchoLog ok "mailutils - ${lang_softwaredependencies_installok}"
-      ConfigPostfix=true
-    else
-      EchoLog error "mailutils - ${lang_softwaredependencies_installfail}"
-    fi
+if ! $ConfigMailserverRole; then
+  EchoLog wait "${lang_wait_installsoftware}"
+  
+  # Install and configure Postfix as MTA
+  if CheckPackage "postfix"; then
+    EchoLog info "postfix - ${lang_softwaredependencies_alreadyinstalled}"
+    if CheckPackage "mailutils"; then EchoLog info "mailutils - ${lang_softwaredependencies_alreadyinstalled}"; fi
   else
-    EchoLog error "postfix - ${lang_softwaredependencies_installfail}"
+    debconf-set-selections <<< "postfix postfix/mailname string $HostName.$DomainName"
+    debconf-set-selections <<< "postfix postfix/main_mailer_type string 'Internet Site'"
+    if apt-get install -y postfix >/dev/null 2>&1; then
+      EchoLog ok "postfix - ${lang_softwaredependencies_installok}"
+      if apt-get install -y mailutils >/dev/null 2>&1; then
+        EchoLog ok "mailutils - ${lang_softwaredependencies_installok}"
+        ConfigPostfix=true
+      else
+        EchoLog error "mailutils - ${lang_softwaredependencies_installfail}"
+      fi
+    else
+      EchoLog error "postfix - ${lang_softwaredependencies_installfail}"
+    fi
   fi
+  
+  # Install Software dependencies 
+  for PACKAGE in fail2ban curl snapd gnupg git ca-certificates apticron smartmontools; do
+    if CheckPackage "${PACKAGE}"; then
+      EchoLog info "${PACKAGE} - ${lang_softwaredependencies_alreadyinstalled}"
+    else
+      if apt-get install -y $PACKAGE >/dev/null 2>&1; then
+        EchoLog ok "${PACKAGE} - ${lang_softwaredependencies_installok}"
+      else
+        EchoLog error "${PACKAGE} - ${lang_softwaredependencies_installfail}"
+      fi
+    fi
+  done
 fi
-
-# Install Software dependencies 
-for PACKAGE in fail2ban curl snapd gnupg git ca-certificates apticron smartmontools; do
-  if CheckPackage "${PACKAGE}"; then
-    EchoLog info "${PACKAGE} - ${lang_softwaredependencies_alreadyinstalled}"
-  else
-    if apt-get install -y $PACKAGE >/dev/null 2>&1; then
-      EchoLog ok "${PACKAGE} - ${lang_softwaredependencies_installok}"
-    else
-      EchoLog error "${PACKAGE} - ${lang_softwaredependencies_installfail}"
-    fi
-  fi
-done
 
 # Do a Full System update and upgrade if this is the Firstrun on this host
 EchoLog wait "${lang_updateupgrade_startup} >>> (apt-get update && apt-get upgrade)"
@@ -174,66 +178,68 @@ fi
 ###############################
 ##       P O S T F I X       ##
 ###############################
-if $ConfigPostfix; then
-  # Configure Postfix with made mail server settings
-  BackupAndRestoreFile backup "/etc/aliases"
-  BackupAndRestoreFile backup "/etc/postfix/main.cf"
-  BackupAndRestoreFile backup "/etc/ssl/certs/ca-certificates.crt"
-  PostfixConfigured=false
-
-  # Change Pistfix configuration to send E-Mails
-  if grep "root:" /etc/aliases; then
-    sed -i "s/^root:.*$/root: $MailServerTo/" /etc/aliases
-  else
-    echo "root: $MailServerTo" >> /etc/aliases
-  fi
-  echo "root $MailServerFrom" >> /etc/postfix/canonical
-  chmod 600 /etc/postfix/canonical
-  echo [$MailServerFQDN]:$MailServerPort "$MailServerUser":"$MailServerPass" >> /etc/postfix/sasl_passwd
-  chmod 600 /etc/postfix/sasl_passwd
-  sed -i "/#/!s/\(relayhost[[:space:]]*=[[:space:]]*\)\(.*\)/\1"[$MailServerFQDN]:"$MailServerPort""/"  /etc/postfix/main.cf
-  if [ $MailServerTLS ]; then
-    postconf smtp_use_tls=yes
-  else
-    postconf smtp_use_tls=no
-  fi
-  if ! grep "smtp_sasl_password_maps" /etc/postfix/main.cf; then
-    postconf smtp_sasl_password_maps=hash:/etc/postfix/sasl_passwd > /dev/null 2>&1
-  fi
-  if ! grep "smtp_tls_CAfile" /etc/postfix/main.cf; then
-    postconf smtp_tls_CAfile=/etc/ssl/certs/ca-certificates.crt > /dev/null 2>&1
-  fi
-  if ! grep "smtp_sasl_security_options" /etc/postfix/main.cf; then
-    postconf smtp_sasl_security_options=noanonymous > /dev/null 2>&1
-  fi
-  if ! grep "smtp_sasl_auth_enable" /etc/postfix/main.cf; then
-    postconf smtp_sasl_auth_enable=yes > /dev/null 2>&1
-  fi 
-  if ! grep "sender_canonical_maps" /etc/postfix/main.cf; then
-    postconf sender_canonical_maps=hash:/etc/postfix/canonical > /dev/null 2>&1
-  fi 
-  postmap /etc/postfix/sasl_passwd > /dev/null 2>&1
-  postmap /etc/postfix/canonical > /dev/null 2>&1
-  systemctl restart postfix  &> /dev/null && systemctl enable postfix  &> /dev/null
-  rm -rf "/etc/postfix/sasl_passwd"
-
-  # Test Postfix settings
-  echo -e "${lang_testpostfix_sendmessage}" | mail -a "From: \"${HostName}\" <${MailServerFrom}>" -s "[${lang_testpostfix_subjectarray^^}] ${lang_testpostfix_subjecttext}" "$MailServerTo"
-  if ! WhipYesNo "${lang_btn_yes}" "${lang_btn_no}" "${lang_testpostfix_whiptitle}" "${lang_testpostfix_whipyesnotext}\n\n${MailServerTo}"; then
-    AlertWhipMessage "${lang_testpostfix_whiptitle}" "${lang_testpostfix_whipalertmessage}"
-    if grep "SMTPUTF8 is required" "/var/log/mail.log"; then
-      if ! grep "smtputf8_enable = no" /etc/postfix/main.cf; then
-        postconf smtputf8_enable=no
-        postfix reload
-      fi
+if ! $ConfigMailserverRole; then
+  if $ConfigPostfix; then
+    # Configure Postfix with made mail server settings
+    BackupAndRestoreFile backup "/etc/aliases"
+    BackupAndRestoreFile backup "/etc/postfix/main.cf"
+    BackupAndRestoreFile backup "/etc/ssl/certs/ca-certificates.crt"
+    PostfixConfigured=false
+  
+    # Change Pistfix configuration to send E-Mails
+    if grep "root:" /etc/aliases; then
+      sed -i "s/^root:.*$/root: $MailServerTo/" /etc/aliases
+    else
+      echo "root: $MailServerTo" >> /etc/aliases
     fi
+    echo "root $MailServerFrom" >> /etc/postfix/canonical
+    chmod 600 /etc/postfix/canonical
+    echo [$MailServerFQDN]:$MailServerPort "$MailServerUser":"$MailServerPass" >> /etc/postfix/sasl_passwd
+    chmod 600 /etc/postfix/sasl_passwd
+    sed -i "/#/!s/\(relayhost[[:space:]]*=[[:space:]]*\)\(.*\)/\1"[$MailServerFQDN]:"$MailServerPort""/"  /etc/postfix/main.cf
+    if [ $MailServerTLS ]; then
+      postconf smtp_use_tls=yes
+    else
+      postconf smtp_use_tls=no
+    fi
+    if ! grep "smtp_sasl_password_maps" /etc/postfix/main.cf; then
+      postconf smtp_sasl_password_maps=hash:/etc/postfix/sasl_passwd > /dev/null 2>&1
+    fi
+    if ! grep "smtp_tls_CAfile" /etc/postfix/main.cf; then
+      postconf smtp_tls_CAfile=/etc/ssl/certs/ca-certificates.crt > /dev/null 2>&1
+    fi
+    if ! grep "smtp_sasl_security_options" /etc/postfix/main.cf; then
+      postconf smtp_sasl_security_options=noanonymous > /dev/null 2>&1
+    fi
+    if ! grep "smtp_sasl_auth_enable" /etc/postfix/main.cf; then
+      postconf smtp_sasl_auth_enable=yes > /dev/null 2>&1
+    fi 
+    if ! grep "sender_canonical_maps" /etc/postfix/main.cf; then
+      postconf sender_canonical_maps=hash:/etc/postfix/canonical > /dev/null 2>&1
+    fi 
+    postmap /etc/postfix/sasl_passwd > /dev/null 2>&1
+    postmap /etc/postfix/canonical > /dev/null 2>&1
+    systemctl restart postfix  &> /dev/null && systemctl enable postfix  &> /dev/null
+    rm -rf "/etc/postfix/sasl_passwd"
+  
+    # Test Postfix settings
     echo -e "${lang_testpostfix_sendmessage}" | mail -a "From: \"${HostName}\" <${MailServerFrom}>" -s "[${lang_testpostfix_subjectarray^^}] ${lang_testpostfix_subjecttext}" "$MailServerTo"
     if ! WhipYesNo "${lang_btn_yes}" "${lang_btn_no}" "${lang_testpostfix_whiptitle}" "${lang_testpostfix_whipyesnotext}\n\n${MailServerTo}"; then
-      AlertWhipMessage "${lang_testpostfix_whiptitle}" "${lang_testpostfix_whipalertmessage2}"
-      BackupAndRestoreFile restore "/etc/aliases"
-      BackupAndRestoreFile restore "/etc/postfix/canonical"
-      BackupAndRestoreFile restore "/etc/postfix/main.cf"
-      BackupAndRestoreFile restore "/etc/ssl/certs/ca-certificates.crt"
+      AlertWhipMessage "${lang_testpostfix_whiptitle}" "${lang_testpostfix_whipalertmessage}"
+      if grep "SMTPUTF8 is required" "/var/log/mail.log"; then
+        if ! grep "smtputf8_enable = no" /etc/postfix/main.cf; then
+          postconf smtputf8_enable=no
+          postfix reload
+        fi
+      fi
+      echo -e "${lang_testpostfix_sendmessage}" | mail -a "From: \"${HostName}\" <${MailServerFrom}>" -s "[${lang_testpostfix_subjectarray^^}] ${lang_testpostfix_subjecttext}" "$MailServerTo"
+      if ! WhipYesNo "${lang_btn_yes}" "${lang_btn_no}" "${lang_testpostfix_whiptitle}" "${lang_testpostfix_whipyesnotext}\n\n${MailServerTo}"; then
+        AlertWhipMessage "${lang_testpostfix_whiptitle}" "${lang_testpostfix_whipalertmessage2}"
+        BackupAndRestoreFile restore "/etc/aliases"
+        BackupAndRestoreFile restore "/etc/postfix/canonical"
+        BackupAndRestoreFile restore "/etc/postfix/main.cf"
+        BackupAndRestoreFile restore "/etc/ssl/certs/ca-certificates.crt"
+      fi
     fi
   fi
 fi
@@ -241,13 +247,15 @@ fi
 ###############################
 ##      A P T I C R O N      ##
 ###############################
-# Configure Postfix with made mail server settings
-if [ ! -f "/etc/apticron/apticron.conf" ]; then
-  if cp /usr/lib/apticron/apticron.conf /etc/apticron/apticron.conf; then
-    sed -i "s/EMAIL=\".*/EMAIL=\"$MailServerTo\"/" /etc/apticron/apticron.conf
-    sed -i "s/# CUSTOM_SUBJECT=\".*/CUSTOM_SUBJECT=\"[${HostName^^}] ${lang_confapticron_customsubject}\"/" /etc/apticron/apticron.conf
-    sed -i "s/# CUSTOM_NO_UPDATES_SUBJECT=\".*/CUSTOM_NO_UPDATES_SUBJECT=\"[${HostName^^}] ${lang_confapticron_customnoupdatessubject}\"/" /etc/apticron/apticron.conf
-    sed -i "s/# CUSTOM_FROM=\".*/CUSTOM_FROM=\"$MailServerFrom\"/" /etc/apticron/apticron.conf
+if ! $ConfigMailserverRole; then
+  # Configure Postfix with made mail server settings
+  if [ ! -f "/etc/apticron/apticron.conf" ]; then
+    if cp /usr/lib/apticron/apticron.conf /etc/apticron/apticron.conf; then
+      sed -i "s/EMAIL=\".*/EMAIL=\"$MailServerTo\"/" /etc/apticron/apticron.conf
+      sed -i "s/# CUSTOM_SUBJECT=\".*/CUSTOM_SUBJECT=\"[${HostName^^}] ${lang_confapticron_customsubject}\"/" /etc/apticron/apticron.conf
+      sed -i "s/# CUSTOM_NO_UPDATES_SUBJECT=\".*/CUSTOM_NO_UPDATES_SUBJECT=\"[${HostName^^}] ${lang_confapticron_customnoupdatessubject}\"/" /etc/apticron/apticron.conf
+      sed -i "s/# CUSTOM_FROM=\".*/CUSTOM_FROM=\"$MailServerFrom\"/" /etc/apticron/apticron.conf
+    fi
   fi
 fi
 
@@ -257,28 +265,31 @@ fi
 #WhipMessage "${lang_configurationcompleted_title}" "${lang_configurationcompleted_message}"
 if WhipYesNo "${lang_btn_yes}" "${lang_btn_no}" "${lang_configurationcompleted_title}" "${lang_configurationcompleted_messageyesno}"; then ConfigRole=true; fi
 
+if $ConfigMailserverRole; then
+  # Mailcow Mailserver
+  if bash <(curl -s https://raw.githubusercontent.com/iThieler/uscp/main/misc/mc.sh); then
+    EchoLog ok "${lang_mailserver_configok}"
+  else
+    EchoLog error "${lang_serverrole_configerror}"
+    if rm -rf "/opt/*"; then
+      EchoLog ok "${lang_serverrole_configundo_ok}"
+    else
+      EchoLog error "${lang_serverrole_configundo_error}"
+    fi
+  fi
+  CleanupAll
+  EchoLog info "${lang_goodbye}"
+  exit 0
+fi
+
 # Select Server Role
 if $ConfigRole; then
   ServerRole=$(whiptail --menu --nocancel --backtitle "${var_whipbacktitle}" --title " ${lang_selectserverrole_title^^} " "\n${lang_selectserverrole_message}" 20 80 10 "${rolelist[@]}" 3>&1 1>&2 2>&3)
+
   if [[ $ServerRole == "dp" ]]; then
     # Config Docker Server with NGINX Proxy Manager
     if bash <(curl -s https://raw.githubusercontent.com/iThieler/uscp/main/misc/dp.sh); then
       EchoLog ok "${lang_dockerserver_configok}"
-    else
-      EchoLog error "${lang_serverrole_configerror}"
-      if rm -rf "/opt/*"; then
-        EchoLog ok "${lang_serverrole_configundo_ok}"
-      else
-        EchoLog error "${lang_serverrole_configundo_error}"
-      fi
-    fi
-    CleanupAll
-    EchoLog info "${lang_goodbye}"
-    exit 0
-  elif [[ $ServerRole == "mc" ]]; then
-    # Mailcow Mailserver
-    if bash <(curl -s https://raw.githubusercontent.com/iThieler/uscp/main/misc/mc.sh); then
-      EchoLog ok "${lang_mailserver_configok}"
     else
       EchoLog error "${lang_serverrole_configerror}"
       if rm -rf "/opt/*"; then
